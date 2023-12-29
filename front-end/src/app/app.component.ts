99d7baa458e3828470fe35cfb77397bdd4d858e5
Higor Oliveira dos Santos <higorosantos@gmail.com>
2023-12-29 19:28:07 -0300

    Primeiros components

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from '../pages/teste/test.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TestComponent],
+  imports: [CommonModule, RouterOutlet, ButtonComponent, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
